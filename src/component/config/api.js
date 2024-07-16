46d66c7deb603978c05941d2e8f06f3529087621
Nitesh <niteshbarnwal8874@gmail.com>
2024-07-16 23:20:23 +0530

    api update

--- src/component/config/api.js
+++ src/component/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const API_URL = "http://localhost:5454";
-export const API_URL = "https://sincere-dream-production.up.railway.app/:5454";
+export const API_URL = "https://sincere-dream-production.up.railway.app:5454";
 
 export const api = axios.create({
     baseURL: API_URL, // Corrected from baseURl to baseURL
